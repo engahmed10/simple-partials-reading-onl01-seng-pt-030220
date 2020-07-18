a768af68a5bc6a9d19b79d6c99ee92026ad734d8
AHMED AFANDY <eng.computer1@yahoo.com>
2020-07-18 07:56:59 +0000

    Automatically backed up by Learn

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,10 +1,10 @@
 class PostsController < ApplicationController
   def index
-    #@posts = Post.all
+    @posts = Post.all
   end
 
   def show
-    @posts = Post.all
+    
     @post = Post.find(params[:id])
     @author=@post.author
   end
